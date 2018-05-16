dcbc63cae8ae47ca8b943e2a752e3746434e4a26
Sourav Datta <sdatta@planview.com>
2018-05-16 13:21:48 +0530

    Add exporter

--- client/main.js
+++ client/main.js
@@ -1,1 +1,2 @@
 import '../imports/ui/body';
+import '../imports/ui/exporter';
---
+++ imports/ui/exporter.js
@@ -0,0 +1,29 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './body.html';
+import { Intent } from '/imports/api/intents';
+
+import { saveAs } from 'file-saver';
+
+Template.exporter.events({
+  'click #trn-export'(event) {
+    const allIntents = Intent.find();
+    let csvData = 'text,intent\n';
+    let blob = null;
+
+    allIntents.forEach((row) => {
+      csvData += `${row.text},${row.intent}\n`;
+    });
+    try {
+      blob = new Blob([csvData], {type: 'text/csv;charset=utf-8'});
+    } catch (e) {
+      alert('Cannot generate data, check console for errors');
+      console.log(e);
+    }
+
+    if (blob) {
+      return saveAs(blob, 'intents_data.csv');
+    }
+  }
+});
