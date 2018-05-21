3ec0f28a930ba3984265a5a43c87c57e781da043
Sourav Datta <sdatta@planview.com>
2018-05-21 16:55:32 +0530

    Replace comma too

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -40,7 +40,7 @@ Template.body.events({
 
       try {
         Intent.insert({
-          text: text.replace(/\n/g, ' '),
+          text: text.replace(/[\n,\,]/g, ' '),
           intent: intent,
           lang: lang
         });
